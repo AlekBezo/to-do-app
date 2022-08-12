cde9d73370c69546699dc9b20c0e137b8f597a76
AlekBezo <aleksandarbezanic@yahoo.com>
2022-08-12 15:10:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: Math.random(),
+      title: "Clean room",
+      isCompleted: false,
+    },
+    {
+      id: Math.random(),
+      title: "Go to the gym",
+      isCompleted: false,
+    },
+    {
+      id: Math.random(),
+      title: "learn javascript",
+      isCompleted: false,
+    },
+  ]);
+  const [inputIsShown, setInputIsShown] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container" className="App">
+      <h1>
+        To-Do List
+        <i
+          className="fa fa-toggle-on"
+          id="btn"
+          aria-hidden="true"
+          onClick={() => {
+            setInputIsShown((prevState) => !prevState);
+          }}
+        ></i>
+      </h1>
+      <input
+        value={inputValue}
+        type="text"
+        placeholder="Add New Todo"
+        className={!inputIsShown && "hideInput"}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            setTodos([
+              ...todos,
+              {
+                id: Math.random(),
+                title: inputValue,
+                isCompleted: false,
+              },
+            ]);
+            setInputValue("");
+          }
+        }}
+      />
+      <ul>
+        {todos.map((firstMapItem) => (
+          <li
+            key={firstMapItem.id}
+            className={firstMapItem.isCompleted ? "completed el" : "el"}
+            onClick={(e) => {
+              e.stopPropagation();
+              setTodos(
+                todos.map((todo) => {
+                  return todo.id === firstMapItem.id
+                    ? {
+                        id: todo.id,
+                        title: todo.title,
+                        isCompleted: !todo.isCompleted,
+                      }
+                    : todo;
+                })
+              );
+            }}
+          >
+            <span
+              className="trash"
+              onClick={(e) => {
+                e.stopPropagation();
+                setTodos(todos.filter((item) => item.id !== firstMapItem.id));
+              }}
+            >
+              <i className="fa fa-trash"></i>
+            </span>{" "}
+            {firstMapItem.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
